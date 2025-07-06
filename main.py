4599ef758597bef89b2e575ed2b251979f48917c
Theifhawk <wchen291@gmail.com>
2025-07-06 01:32:16 -0400

    fiddled around with beautifulSoap

--- main.py
+++ main.py
@@ -6,10 +6,19 @@ def main():
 
 
 def scrape():
-    url = "https://example.com/"
+    url = "https://archidekt.com/decks/6122714/sand_in_my_eyes"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
+    #title = soup.select_one('h1').text
+    #text = soup.select_one('p').text
+    #link = soup.select_one('a').get('href')
+    #print(title)
+    #print(text)
+    #print(link)
     print(soup)
+
+    #for link in soup.find_all('a'):
+        #print(link.get('href'))
 
 
 if __name__ == "__main__":
