f01b65696f03066426fae77b2b5db7b537dd0c4d
Theifhawk <wchen291@gmail.com>
2025-09-13 22:07:34 -0400

    Created loop to find the A tag, which held the titles of One Piece Episodes

--- main.py
+++ main.py
@@ -10,13 +10,24 @@ def scrape():
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     tags = soup.find_all('table', class_="collapsible collapsed")
+    # These will be the tables
     episodes = []
     for tag in tags:
         episodes.append(tag)
-        
     
+    episode_titles = []
+    # For each table, find the A tag
     for episode in episodes:
-        print(episode)
+        print("\n")
+        if episode:
+            for row in episode.find_all('tr'):
+                for cell in row.find_all(['td', 'th']):
+                    target_tag = cell.find('a')
+                    if target_tag:
+                        # print(f"Found tag: {target_tag.name} with text: {target_tag.get_text()}")
+                        episode_titles.append(target_tag.get_text())
+
+    print(episode_titles)
 
 
 
