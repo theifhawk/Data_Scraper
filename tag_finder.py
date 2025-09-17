be21fbd2804e41a869cde4f44947bb62fdf897a6
Theifhawk <wchen291@gmail.com>
2025-09-17 16:05:27 -0400

    Found the Background color

--- tag_finder.py
+++ tag_finder.py
@@ -33,15 +33,23 @@ def find_ep_number(episodes):
     for episode in episodes:
         if episode:
             tag_td = episode.find_all('td')
-            print()
-            print("ep num is:", tag_td[0].get_text())
-            print("air date is:", tag_td[1].get_text())
-
             tag_a = episode.find_all('a')
-            print("Eng Title: ", tag_a[0].get_text())
-
             tag_th = episode.find('th')
             contents = tag_th.contents
+            style_attr = tag_th['style']
+
+            style_dict = dict(
+                item.strip().split(':', 1)
+                for item in style_attr.split(';')
+                    if ':' in item
+                )
+            background_value = style_dict.get('background')
+
+            print()
+            print(background_value)
+            print("ep num is:", tag_td[0].get_text())
+            print("air date is:", tag_td[1].get_text())
+            print("Eng Title: ", tag_a[0].get_text())
             print("Jap Title: ", contents[2])
 
 
