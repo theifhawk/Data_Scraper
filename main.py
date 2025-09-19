f72b216f0b19326d29e5cb7e5c38fabbe4c7efe7
Theifhawk <wchen291@gmail.com>
2025-09-19 00:55:22 -0400

    Created new file to hold all the links for all sagas, everything works except final saga

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import requests
 import tag_finder
+import op_eps
 from bs4 import BeautifulSoup
 import pandas as pd
 
+east_blue = "https://onepiece.fandom.com/wiki/Episode_Guide/East_Blue_Saga" # good
+arabasta = "https://onepiece.fandom.com/wiki/Episode_Guide/Arabasta_Saga" # good
+sky_island = "https://onepiece.fandom.com/wiki/Episode_Guide/Sky_Island_Saga" # good
+water_7 = "https://onepiece.fandom.com/wiki/Episode_Guide/Water_7_Saga" # good
+thriller_bark = "https://onepiece.fandom.com/wiki/Episode_Guide/Thriller_Bark_Saga" # good 
+summit_war = "https://onepiece.fandom.com/wiki/Episode_Guide/Summit_War_Saga" # good
+fishman = "https://onepiece.fandom.com/wiki/Episode_Guide/Fish-Man_Island_Saga" # good
+dressrosa = "https://onepiece.fandom.com/wiki/Episode_Guide/Dressrosa_Saga" # good
+whole_cake = "https://onepiece.fandom.com/wiki/Episode_Guide/Whole_Cake_Island_Saga" # good
+wano = "https://onepiece.fandom.com/wiki/Episode_Guide/Wano_Country_Saga" # good
+final = "https://onepiece.fandom.com/wiki/Episode_Guide/Final_Saga"
 
 def scrape():
-    url = "https://onepiece.fandom.com/wiki/Episode_Guide/East_Blue_Saga"
+    url = "https://onepiece.fandom.com/wiki/Episode_Guide/Final_Saga"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     tags = soup.find_all('table', class_="collapsible collapsed")
@@ -15,6 +27,23 @@ def scrape():
     tag_finder.find_info(episodes)
     return(tag_finder.find_info(episodes))
 
+def op_scrape():
+    episodes_url = op_eps.together()
+    episodes = []
+
+    for url in episodes_url:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        tags = soup.find_all('table', class_="collapsible collapsed")
+        for tag in tags:
+            episodes.append(tag)
+
+    
+    all = tag_finder.find_info(episodes)
+
+    print(all)
+
+
 def main():
     OP_table = ['Episode Number', 'English Title' , 'Japanese Title', 'Release Date', 'Color']
     OP_data = scrape()
@@ -22,7 +51,7 @@ def main():
     for row in OP_data:
         length = len(df)
         df.loc[length] = row
-    df.to_csv('output.csv', index=False)
+    #df.to_csv('output.csv', index=False)
     print(df)
 
 if __name__ == "__main__":
---
+++ op_eps.py
@@ -0,0 +1,29 @@
+
+
+east_blue = "https://onepiece.fandom.com/wiki/Episode_Guide/East_Blue_Saga"
+arabasta = "https://onepiece.fandom.com/wiki/Episode_Guide/Arabasta_Saga"
+sky_island = "https://onepiece.fandom.com/wiki/Episode_Guide/Sky_Island_Saga"
+water_7 = "https://onepiece.fandom.com/wiki/Episode_Guide/Water_7_Saga"
+thriller_bark = "https://onepiece.fandom.com/wiki/Episode_Guide/Thriller_Bark_Saga"
+summit_war = "https://onepiece.fandom.com/wiki/Episode_Guide/Summit_War_Saga"
+fishman = "https://onepiece.fandom.com/wiki/Episode_Guide/Fish-Man_Island_Saga"
+dressrosa = "https://onepiece.fandom.com/wiki/Episode_Guide/Dressrosa_Saga"
+whole_cake = "https://onepiece.fandom.com/wiki/Episode_Guide/Whole_Cake_Island_Saga"
+wano = "https://onepiece.fandom.com/wiki/Episode_Guide/Wano_Country_Saga"
+final = "https://onepiece.fandom.com/wiki/Episode_Guide/Final_Saga"
+
+def together():
+    all_links = []
+    all_links.append(str(east_blue))
+    all_links.append(arabasta)
+    all_links.append(sky_island)
+    all_links.append(water_7)
+    all_links.append(thriller_bark)
+    all_links.append(summit_war)
+    all_links.append(fishman)
+    all_links.append(dressrosa)
+    all_links.append(whole_cake)
+    all_links.append(wano)
+    all_links.append(final)
+
+    return(all_links)
